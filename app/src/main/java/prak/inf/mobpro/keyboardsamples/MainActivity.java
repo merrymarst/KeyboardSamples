0501813c1c1f4aee14bbc215e16efa7c27000376
merrymarst <merrymetoy28@gmail.com>
2017-12-25 22:32:52 +0700

    tugas mopro

---
+++ app/src/main/java/prak/inf/mobpro/keyboardsamples/MainActivity.java
@@ -0,0 +1,26 @@
+package prak.inf.mobpro.keyboardsamples;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import	android.widget.EditText;
+import	android.view.View;
+import	android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    public void showText(View view) {
+        EditText editText = (EditText) findViewById(R.id.editText_main);
+        if (editText != null) {
+            String showString = editText.getText().toString();
+            Toast.makeText(this, showString, Toast.LENGTH_SHORT).show();
+        }
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+}
